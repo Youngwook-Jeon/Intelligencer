3eff044859d2aa225e16847e2ccf85279fd9ac2e
Young wook Jeon <ela87@naver.com>
2021-01-01 18:12:12 +0900

    first commit

---
+++ intelligencer-server/src/main/java/com/young/intelligencer/IntelligencerApplication.java
@@ -0,0 +1,13 @@
+package com.young.intelligencer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligencerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligencerApplication.class, args);
+    }
+
+}
